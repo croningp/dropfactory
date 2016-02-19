a90fe754d15f27d7c938a6e8b3f5d1ce8295f994
Jonathan Grizou <jonathan.grizou@glasgow.ac.uk>
2016-02-19 16:41:03 +0000

    was wrong firmware saved

--- software/arduino/firmware_XYZ/firmware_XYZ.ino
+++ software/arduino/firmware_XYZ/firmware_XYZ.ino
@@ -7,22 +7,22 @@ CommandManager cmdMng;
 #include <LinearAccelStepperActuator.h>
 #include <CommandLinearAccelStepperActuator.h>
 
-AccelStepper stp_G1(AccelStepper::DRIVER, 54, 55);
-CommandLinearAccelStepperActuator G1(stp_G1, 3, 38);
+AccelStepper stp_X(AccelStepper::DRIVER, 54, 55);
+CommandLinearAccelStepperActuator X(stp_X, 3, 38);
 
-AccelStepper stp_G2(AccelStepper::DRIVER, 60, 61);
-CommandLinearAccelStepperActuator G2(stp_G2, 14, 56);
+AccelStepper stp_Y(AccelStepper::DRIVER, 60, 61);
+CommandLinearAccelStepperActuator Y(stp_Y, 14, 56);
 
-AccelStepper stp_S1(AccelStepper::DRIVER, 26, 28);
-CommandLinearAccelStepperActuator S1(stp_S1, 18, 24);
+AccelStepper stp_Z(AccelStepper::DRIVER, 26, 28);
+CommandLinearAccelStepperActuator Z(stp_Z, 18, 24);
 
 void setup()
 {
   Serial.begin(115200);
 
-  G1.registerToCommandManager(cmdMng, "G1");
-  G2.registerToCommandManager(cmdMng, "G2");
-  S1.registerToCommandManager(cmdMng, "S1");
+  X.registerToCommandManager(cmdMng, "X");
+  Y.registerToCommandManager(cmdMng, "Y");
+  Z.registerToCommandManager(cmdMng, "Z");
 
 
   cmdMng.init();
@@ -32,4 +32,3 @@ void loop()
 {
   cmdMng.update();
 }
-
