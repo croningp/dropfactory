2b0859df954da4841bc404e69787bf2399aa7910
Jonathan Grizou <jonathan.grizou@glasgow.ac.uk>
2016-04-18 15:26:03 +0100

    updated firmware

--- software/arduino/firmware_tools/firmware_tools.ino
+++ software/arduino/firmware_tools/firmware_tools.ino
@@ -16,6 +16,16 @@ CommandLinearAccelStepperActuator G2(stp_G2, 14, 56);
 AccelStepper stp_S1(AccelStepper::DRIVER, 26, 28);
 CommandLinearAccelStepperActuator S1(stp_S1, 18, 24);
 
+AccelStepper stp_S2(AccelStepper::DRIVER, 36, 34);
+CommandLinearAccelStepperActuator S2(stp_S2, 19, 30);
+
+#include <Servo.h>
+#include <CommandServo.h>
+CommandServo cmdServo(17);
+
+#include <CommandAnalogWrite.h>
+CommandAnalogWrite aw(16);
+
 void setup()
 {
   Serial.begin(115200);
@@ -23,6 +33,9 @@ void setup()
   G1.registerToCommandManager(cmdMng, "G1");
   G2.registerToCommandManager(cmdMng, "G2");
   S1.registerToCommandManager(cmdMng, "S1");
+  S2.registerToCommandManager(cmdMng, "S2");
+  cmdServo.registerToCommandManager(cmdMng, "S3");
+  aw.registerToCommandManager(cmdMng, "A1");
 
 
   cmdMng.init();
