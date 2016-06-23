6b4fc1f62f318dcdbe9b74e6e9214dd3ce1e10b4
Jonathan Grizou <jonathan.grizou@glasgow.ac.uk>
2016-06-23 17:08:01 +0100

    email notificatio working

---
+++ software/tools/email_notification.py
@@ -0,0 +1,35 @@
+import os
+import inspect
+HERE_PATH = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+
+import json
+
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+
+CREDENTIALS_FILE = os.path.join(HERE_PATH, 'email_credentials.json')
+
+def read_json_file(filename):
+    with open(filename) as f:
+        return json.load(f)
+
+
+def send_notification(toaddr, subject, body):
+
+    credentials = read_json_file(CREDENTIALS_FILE)
+    fromaddr = credentials['user']
+    password = credentials['password']
+
+    msg = MIMEMultipart()
+    msg['From'] = fromaddr
+    msg['To'] = toaddr
+    msg['Subject'] = subject
+    msg.attach(MIMEText(body, 'plain'))
+
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(fromaddr, password)
+    text = msg.as_string()
+    server.sendmail(fromaddr, toaddr, text)
+    server.quit()
