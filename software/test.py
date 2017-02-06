fb2b6ab24e7d3f382f6caf272b48615dc6dd14d0
Jonathan Grizou <jonathan.grizou@glasgow.ac.uk>
2017-02-06 16:02:55 +0000

    nicer tests for demos

--- software/test.py
+++ software/test.py
@@ -17,6 +17,7 @@ from tools.xp_watcher import XPWatcher
 
 from tools.xp_maker import add_XP_to_pool_folder
 
+# movement
 oil_ratios = {
     "dep": 0.92019434642566544,
     "octanol": 0.7492601802279919,
@@ -24,27 +25,30 @@ oil_ratios = {
     "pentanol": 0.84013885086802531
 }
 
-oil_ratios_2 = {
-    "dep": 1.0,
-    "octanol": 0.0,
-    "octanoic": 0.0,
-    "pentanol": 0.0
-}
 
-oil_ratios_3 =  {
-    "dep": 0.0,
-    "octanol": 0.57284714859204744,
-    "octanoic": 0.91965021960068283,
-    "pentanol": 0.56743951850035534
-}
+#sink
+# oil_ratios_2 = {
+#     "dep": 1.0,
+#     "octanol": 0.0,
+#     "octanoic": 0.0,
+#     "pentanol": 0.0
+# }
+
+# wobble
+# oil_ratios_3 =  {
+#     "dep": 0.0,
+#     "octanol": 0.57284714859204744,
+#     "octanoic": 0.91965021960068283,
+#     "pentanol": 0.56743951850035534
+# }
 
 
 pool_folder = os.path.join(HERE_PATH, 'test_pool_folder')
 
-for _ in range(5):
+for _ in range(15):
     add_XP_to_pool_folder(oil_ratios, pool_folder)
-    add_XP_to_pool_folder(oil_ratios_2, pool_folder)
-    add_XP_to_pool_folder(oil_ratios_3, pool_folder)
+    # add_XP_to_pool_folder(oil_ratios_2, pool_folder)
+    # add_XP_to_pool_folder(oil_ratios_3, pool_folder)
 
 watcher = XPWatcher(manager, pool_folder)
 
